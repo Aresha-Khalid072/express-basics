502c4649f0527df4db4991ffef4543372120634f
Aresha-Khalid072 <areshakhalid072@gmail.com>
2026-07-01 18:16:24 +0500

    error handling

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
 import express from 'express';
 
-const app=express();
+const app = express();
 
-app.get ("/", (req,res)=>{
- res.send("AK started Backend");
-})
+app.use((req, res, next) => {
+  console.log("Middleware is started");
+  next();
+});
 
-app.get("/profile", (req,res)=>{
-    res.send("AK G Welcome!");
-})
+app.get("/", (req, res) => {
+  res.send("AK started Backend");
+});
 
+app.get("/profile", (req, res) => {
+  res.send("AK G Welcome!");
+});
 
-app.listen(3000);
+app.get("/about", (req, res) => {
+  res.send("AK G is on About Page");
+});
+
+app.get("/home", (req, res, next) => {
+  return next(new Error("Not implemented"));
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("something went wrong");
+});
+
+app.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
